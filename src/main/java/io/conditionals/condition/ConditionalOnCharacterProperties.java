04dfd5f883297731edab96f499144e3c8e1b6048
Maksym Uimanov <uimanovmaks@gmail.com>
2026-03-02 20:18:28 +0100

    Introduce `ConditionalOnCharacterProperty` and `ConditionalOnCharacterProperties` annotations with `OnCharacterPropertyCondition` implementation.

---
+++ src/main/java/io/conditionals/condition/ConditionalOnCharacterProperties.java
@@ -0,0 +1,13 @@
+package io.conditionals.condition;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Conditional(OnCharacterPropertyCondition.class)
+public @interface ConditionalOnCharacterProperties {
+    ConditionalOnCharacterProperty[] value();
+}
---
+++ src/main/java/io/conditionals/condition/ConditionalOnCharacterProperty.java
@@ -0,0 +1,27 @@
+package io.conditionals.condition;
+
+import io.conditionals.condition.dto.NumericMatchType;
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Conditional(OnCharacterPropertyCondition.class)
+@Repeatable(ConditionalOnCharacterProperties.class)
+public @interface ConditionalOnCharacterProperty {
+    String[] value() default {};
+
+    String prefix() default "";
+
+    String[] name() default {};
+
+    double havingValue() default 0;
+
+    boolean not() default false;
+
+    NumericMatchType matchType() default NumericMatchType.EQUALS;
+
+    boolean matchIfMissing() default false;
+}
